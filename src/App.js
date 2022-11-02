48fe5f885db9668bb8d223b536fb3faf7742f1ac
maksoon_eu <baranovmaks05@gmail.com>
2022-11-02 13:45:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useTelegram } from '../../hooks/useTelegram';
+import { useTelegram } from './hooks/useTelegram';
 import './App.css';
 
 function App() {
